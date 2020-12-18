7d1c784ba261abfaf2a1449f407dae3fbd042300
Arif Cagirici <acagirici23@gmail.com>
2020-12-18 02:11:47 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,4 +1,17 @@
 class CreateArtists < ActiveRecord::Migration[5.2]
+  
+  sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS artists (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  genre TEXT,
+  age INTEGER,
+  hometown TEXT
+  )
+SQL
+ 
+ActiveRecord::Base.connection.execute(sql)
+
   def up
   end
  
